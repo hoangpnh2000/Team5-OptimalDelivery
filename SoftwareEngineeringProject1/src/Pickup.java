4efb6162a7a2f7a5a5267b653034985d8b2fb71d
By161 <92064135+By161@users.noreply.github.com>
2021-10-18 22:26:15 -0400

    Update Pickup.java

--- SoftwareEngineeringProject1/src/Pickup.java
+++ SoftwareEngineeringProject1/src/Pickup.java
@@ -2,11 +2,11 @@ public class Pickup {
     public Pickup() {
     }
     public boolean PickedUp = false;
-    public String PickUppoint = "P";
+    public String PickUpPoint = "P";
 
     public static void main(String args[]) {
         Pickup pick1 = new Pickup();
         System.out.println(pick1.PickedUp);
-        System.out.println(pick1.PickUppoint);
+        System.out.println(pick1.PickUpPoint);
     }
 }
