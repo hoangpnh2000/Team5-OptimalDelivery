041442d2943eebe501ae98f9ceaf8d801c476452
hoangpnh2000 <45943786+hoangpnh2000@users.noreply.github.com>
2021-10-19 18:25:42 -0400

    Add files via upload

---
+++ SoftwareEngineeringProject1/Main.java
@@ -0,0 +1,35 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		JFrame window= new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("2D Adventure");
+		
+		Panel panel= new Panel();
+		window.add(panel);
+		
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);*/
+		
+		FirstPage page1 = new FirstPage();
+		page1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		System.out.println("Opening First Page");
+		
+		
+		//panel.startRunThread();
+
+		}
+
+}
