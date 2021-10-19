971ee7e4f3ee8f6021efcb2d8645778e675524ab
hoangpnh2000 <hoangpnh2000@yahoo.com>
2021-10-19 13:35:29 -0400

    Update Main.java

--- SoftwareEngineeringProject1/src/Main.java
+++ SoftwareEngineeringProject1/src/Main.java
@@ -1,7 +1,30 @@
-package Team5-OptimalDelivery;
+package main;
+
+import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String args[]){
-		new ListDelivery().run();
-	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		JFrame window= new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("2D Adventure");
+		
+		Panel panel= new Panel();
+		window.add(panel);
+		
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		//panel.startRunThread();
+
+		}
+
 }
