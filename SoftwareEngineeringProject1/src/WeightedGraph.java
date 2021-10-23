3656ca2c69812a03aa839067a1d32c215fd557af
howardlai471 <howardlai471@yahoo.com>
2021-10-23 12:43:01 -0400

    Updated WeightedGraph
    Comments and  cleanup

--- SoftwareEngineeringProject1/src/WeightedGraph.java
+++ SoftwareEngineeringProject1/src/WeightedGraph.java
@@ -2,57 +2,56 @@ import java.util.LinkedList;
 
 public class WeightedGraph {
     static class Edge {
-        int source;
-        int destination;
-        int weight;
+        int src, dest, distance;
 
-        public Edge(int source, int destination, int weight) {
-            this.source = source;
-            this.destination = destination;
-            this.weight = weight;
+        //Constructor
+        public Edge(int src, int dest, int distance) {
+            this.src = src;
+            this.dest = dest;
+            this.distance = distance;
         }
     }
 
     static class Graph {
         int vertices;
+        //Initialize adj list for all vertices
         LinkedList<Edge> [] adjacencylist;
-
         Graph(int vertices) {
             this.vertices = vertices;
             adjacencylist = new LinkedList[vertices];
-            //initialize adjacency lists for all the vertices
             for (int i = 0; i <vertices ; i++) {
                 adjacencylist[i] = new LinkedList<>();
             }
         }
 
-        public void addEgde(int source, int destination, int weight) {
-            Edge edge = new Edge(source, destination, weight);
-            adjacencylist[source].addFirst(edge); //for directed graph
+        public void addEdge(int source, int dest, int distance) {
+            Edge edge = new Edge(source, dest, distance);
+            adjacencylist[source].addFirst(edge);
         }
 
         public void printGraph(){
             for (int i = 0; i <vertices ; i++) {
                 LinkedList<Edge> list = adjacencylist[i];
                 for (int j = 0; j <list.size() ; j++) {
-                    System.out.println("vertex-" + i + " is connected to " +
-                            list.get(j).destination + " with weight " + list.get(j).weight);
+                    String temp = "Source vertex " + i;
+                    while (temp.length() < 25){
+                        temp+=" ";
+                    }
+                    temp += "Destination vertex " + list.get(j).dest;
+                    while (temp.length() < 55){
+                        temp+=" ";
+                    }
+                    temp += "Distance " + list.get(j).distance;
+                    System.out.println(temp);
                 }
             }
         }
     }
     public static void main(String[] args) {
-        int vertices = 6;
+        int vertices = 230;
         Graph graph = new Graph(vertices);
-        graph.addEgde(0, 1, 4);
-        graph.addEgde(0, 2, 3);
-        graph.addEgde(1, 3, 2);
-        graph.addEgde(1, 2, 5);
-        graph.addEgde(2, 3, 7);
-        graph.addEgde(3, 4, 2);
-        graph.addEgde(4, 0, 4);
-        graph.addEgde(4, 1, 4);
-        graph.addEgde(4, 5, 6);
+        graph.addEdge(23, 200, 4);
+        graph.addEdge(200, 23, 7);
         graph.printGraph();
     }
 }
